8f579e1ede55f279d77d9ac1f8cc2a044b1056a1
wissam alshohof <wissam.alshohof@gmail.com>
2023-04-26 23:22:36 +0400

    v0.0.1

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+production :true,
+APP_VERSION: process.env['APP_VERSION']
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+production :false,
+APP_VERSION: '0.0.1',
+}
